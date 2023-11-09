ff9a60538e0a071763c54607081357b7e0821c3a
OualidBenkarim <bmoc07@gmail.com>
2023-11-09 16:35:17 -0500

    update scripts accordingly - breaking changes

--- functions/init.sh
+++ functions/init.sh
@@ -1,8 +1,9 @@
-#/bin/bash
+#!/bin/bash
 
 # ----------------------------------------------------------------------------------------------- #
 # -------------------------------- Software and SGE configuration ------------------------------- #
 # ----------------------------------------------------------------------------------------------- #
+local_threads=$1
 
 # Permissions
 umask 002
@@ -15,26 +16,26 @@ export OLD_PATH=$PATH
 # ----------------------------------------------------------------------------------------------- #
 # User defined PATHS
 # AFNI
-export AFNIDIR="/data/mica1/01_programs/afni-20.2.06"
+#export AFNIDIR="/data/mica1/01_programs/afni-20.2.06"
 # ANTS
 export ANTSPATH="/data/mica1/01_programs/ants-2.3.4/bin"
 # Workbench
-export workbench_path="/data/mica1/01_programs/workbench-1.4.2/bin_linux64"
+export WORKBENCH_PATH="/data/mica1/01_programs/workbench-1.4.2/bin_linux64"
 # FreeSurfer and fastsurfer
 export FREESURFER_HOME="/data/mica1/01_programs/freesurfer-7.3.2"
 export FASTSURFER_HOME="/data_/mica1/01_programs/fastsurfer"
-export fs_licence="/data_/mica1/01_programs/freesurfer-7.3.2/license.txt"
+#export fs_licence="/data_/mica1/01_programs/freesurfer-7.3.2/license.txt"
 unset TMPDIR
 
 # Remove any other instances of dependencies from the PATH
 # ANTS
-PATH=$(IFS=':';p=($PATH);unset IFS;p=(${p[@]%%*ants*});IFS=':';echo "${p[*]}";unset IFS)
+PATH=$(IFS=':';p=("$PATH");unset IFS;p=("${p[@]%%*ants*}");IFS=':';echo "${p[*]}";unset IFS)
 # Workbench binaries
-PATH=$(IFS=':';p=($PATH);unset IFS;p=(${p[@]%%*workbench*});IFS=':';echo "${p[*]}";unset IFS)
+PATH=$(IFS=':';p=("$PATH");unset IFS;p=("${p[@]%%*workbench*}");IFS=':';echo "${p[*]}";unset IFS)
 # Any other python/conda configuration from the PATH 
-PATH=$(IFS=':';p=($PATH);unset IFS;p=(${p[@]%%*conda*});IFS=':';echo "${p[*]}";unset IFS)
+PATH=$(IFS=':';p=("$PATH");unset IFS;p=("${p[@]%%*conda*}");IFS=':';echo "${p[*]}";unset IFS)
 # Byebye LD_LIBRARY_PATH variable
-LD_LIBRARY_PATH=$(IFS=':';p=($LD_LIBRARY_PATH);unset IFS;p=(${p[@]%%*conda*});IFS=':';echo "${p[*]}";unset IFS)
+LD_LIBRARY_PATH=$(IFS=':';p=("$LD_LIBRARY_PATH");unset IFS;p=("${p[@]%%*conda*}");IFS=':';echo "${p[*]}";unset IFS)
 
 # FreeSurfer configuration
 source "${FREESURFER_HOME}/FreeSurferEnv.sh"
@@ -47,12 +48,11 @@ conda3_bin=/data/mica1/01_programs/micapipe-v0.2.0_conda/conda3/bin/
 source /data/mica1/01_programs/micapipe-v0.2.0_conda/conda3/etc/profile.d/conda.sh
 
 # Export new PATH with al the necessary binaries
-export PATH="${ANTSPATH}:${workbench_path}:${FREESURFER_HOME}/bin/:${FASTSURFER_HOME}:${conda3_bin}:${PATH}"
+export PATH="${ANTSPATH}:${WORKBENCH_PATH}:${FREESURFER_HOME}/bin/:${FASTSURFER_HOME}:${conda3_bin}:${PATH}"
 conda activate /data/mica1/01_programs/micapipe-v0.2.0_conda/micapipe
 
 # Add the number of threads to use here. Note that this is overwritten by
 # $NSLOTS if it exists (i.e. when running on SGE).
-local_threads="$1"
 if [[ -z $local_threads ]]; then export local_threads=10; fi
 
 # Set basic global variables
