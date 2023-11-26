dd0536a9a4ce667364938872f822427d09b177f6
OualidBenkarim <bmoc07@gmail.com>
2023-11-26 17:50:08 -0500

    More code refactoring & task/structure-wise sanity checks

--- functions/run_proc.sh
+++ functions/run_proc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Subcortical feature mapping
+# Feature mapping for cortex, subcortex, and hippocampus
 
 
 if [[ -z ${ZBRAINS} ]]; then
@@ -19,7 +19,7 @@ umask 003
 while [ "$#" -gt 0 ]; do
   case "$1" in
     --structure)
-      if [[ " ${LIST_STRUCTURES[*]} " != *" $2 "* ]]; then Error "Invalid argument for $1: $2"; exit 1; fi
+      if [[ " ${LIST_STRUCTURES[*]} " != *" $2 "* ]]; then SHOW_ERROR "Invalid argument for $1: $2"; exit 1; fi
       structure="$2"
       shift 2
       ;;
@@ -48,8 +48,8 @@ while [ "$#" -gt 0 ]; do
       shift 2
       ;;
     *)
-      PROC=$*
-      break
+      SHOW_ERROR "Error: Unknown option '$1'"
+      exit 1
       ;;
   esac
 done
@@ -58,46 +58,31 @@ done
 declare -A mandatory=([struct]=$structure [features]=$comma_separated_features [tmp]=$tmp_dir [threads]=$threads)
 for k in "${!mandatory[@]}"; do
   if [ -z "${mandatory[${k}]}" ]; then
-    Error "Mandatory argument is missing: -$k"
+    SHOW_ERROR "Mandatory argument is missing: -$k"
     exit 1
   fi
 done
 
 if [[ "$structure" != "subcortex" ]]; then
-  [[ -z "$fwhm" ]] && Error "Mandatory argument is missing: -fwhm" && exit 1;
-  [[ -z "$comma_separated_resolutions" ]] && Error "Mandatory argument is missing: -resolution" && exit 1;
+  [[ -z "$fwhm" ]] && SHOW_ERROR "Mandatory argument is missing: -fwhm" && exit 1;
+  [[ -z "$comma_separated_resolutions" ]] && SHOW_ERROR "Mandatory argument is missing: -resolution" && exit 1;
 fi
-
-if [ -z "$PROC" ]; then echo "Error: Mandatory arguments are missing."; exit 1; fi
 
 
 #------------------------------------------------------------------------------#
 # Initialization of pertinent scripts and functions
-
-# qsub configuration
-if [ "$PROC" = "qsub-MICA" ] || [ "$PROC" = "qsub-all.q" ];then
-    source "${ZBRAINS}/functions/init.sh" "$threads"
-fi
 export OMP_NUM_THREADS=$threads
-
-# source utilities
-source "$ZBRAINS/functions/utilities.sh"
-source "$ZBRAINS/config.cfg"
 
 
 #------------------------------------------------------------------------------#
 declare -A map_struct=([cortex]=Cortical [subcortex]=Subcortical [hippocampus]=Hippocampal)
 
-Title "${map_struct[${structure}]} feature mapping\n\t\tz-brains $VERSION, $PROC"
-zbrains_software
-bids_print "$structure"
-Info "wb_command will use $OMP_NUM_THREADS threads"
+Title "${map_struct[${structure}]} feature mapping\n\t\tz-brains $VERSION"
 
 
 #------------------------------------------------------------------------------#
-# Initialize timer and counter of processed files
+# Initialize timer
 SECONDS=0
-n_steps=0
 
 
 #------------------------------------------------------------------------------#
@@ -123,15 +108,15 @@ do_cortex_mapping() {
   local resolution=$2
 
   # Input & output locations
-  surf_dir="${DIR_CONTE69}"
-  input_dir="${DIR_MAPS}"
-  output_dir=${SUBJECT_OUT}/${FOLDER_MAPS}/${FOLDER_CTX}
+  surf_dir=${SUBJECT_MICAPIPE_DIR}/surf
+  input_dir=${SUBJECT_MICAPIPE_DIR}/maps
+  output_dir=${SUBJECT_OUTPUT_DIR}/${FOLDER_MAPS}/${FOLDER_CTX}
 
   # Mappings from features names to the way they appear in the input and output filenames
   declare -A map_input=([thickness]=thickness [flair]=white_flair [adc]=white_ADC [fa]=white_FA [qt1]=white_T1map)
   declare -A map_output=([thickness]=thickness [flair]=flair [adc]=ADC [fa]=FA [qt1]=T1map)
 
-  Info "Map '${feat}' to cortex [resolution=${resolution}]"
+  SHOW_INFO "Map '${feat}' to cortex [resolution=${resolution}]"
 
   # feat name in filenames
   input_feat="${map_input[${feat}]}"
@@ -147,28 +132,28 @@ do_cortex_mapping() {
 
     # Check if file exists
     if [[ ! -f "${input_file}" ]]; then
-#      Note "Processing of '${feat}' requested for ${BIDS_ID} but did not find pre-processed file: skipping"
-      Warning "Subject ${BIDS_ID}: '${feat}' not available. Skipping..."
+      SHOW_WARNING "Subject ${BIDS_ID}: '${feat}' not available. Skipping..."
       break
     fi
 
     # Perform mapping
-    Do_cmd wb_command -metric-smoothing "${surf_file}" "${input_file}" "${fwhm}" "${output_file}";
-    [[ -f "$output_file" ]] && n=$((n + 1)) && n_steps=$((n_steps + 1))
+    DO_CMD wb_command -metric-smoothing "${surf_file}" "${input_file}" "${fwhm}" "${output_file}";
+    [[ -f "$output_file" ]] && n=$((n + 1))
   done
 
-  [[ $n -eq 2 ]] && Note "Subject ${BIDS_ID}: '${feat}' [resolution=${resolution}] successfully mapped to cortex";
+  [[ $n -eq 2 ]] && SHOW_NOTE "Subject ${BIDS_ID}: '${feat}' [resolution=${resolution}] successfully mapped to cortex";
 }
 
 do_subcortex_mapping (){
   local feat=$1
 
-  Info "Map '${feat}' to subcortical structures"
+  SHOW_INFO "Map '${feat}' to subcortical structures"
 
   # Input & output locations
-  vol_stats_file="${DIR_SUBJSURF}/stats/aseg.stats"
-  input_dir="${DIR_MAPS}"
-  output_dir=${SUBJECT_OUT}/${FOLDER_MAPS}/${FOLDER_SCTX}
+  vol_stats_file="${SUBJECT_SURF_DIR}/stats/aseg.stats"
+  seg_file=${SUBJECT_MICAPIPE_DIR}/parc/${BIDS_ID}_space-nativepro_T1w_atlas-subcortical.nii.gz
+  input_dir=${SUBJECT_MICAPIPE_DIR}/maps
+  output_dir=${SUBJECT_OUTPUT_DIR}/${FOLDER_MAPS}/${FOLDER_SCTX}
 
   # Mappings from features names to the way they appear in the input and output filenames
   declare -A map_input=([flair]='map-flair' [adc]='model-DTI_map-ADC' [fa]='model-DTI_map-FA' [qt1]='map-T1map')
@@ -181,18 +166,17 @@ do_subcortex_mapping (){
   if [[ "$feat" != "volume" ]]; then
     input_file="${input_dir}/${BIDS_ID}_space-nativepro_${map_input[$feat]}.nii.gz"
     if [[ ! -f "${input_file}" ]]; then
-#      Note "Processing of '${feat}' requested but did not find pre-processed file: skipping"; return
-      Warning "Subject ${BIDS_ID}: '${feat}' not available. Skipping...";  return
+      SHOW_WARNING "Subject ${BIDS_ID}: '${feat}' not available. Skipping...";  return
     fi
 
-    mapfile -t a < <(ImageIntensityStatistics 3 "${input_file}" "${T1FAST_SEG}" | tail -n +2 | awk '{ print $1 "=" $2 }')
+    mapfile -t a < <(ImageIntensityStatistics 3 "${input_file}" "${seg_file}" | tail -n +2 | awk '{ print $1 "=" $2 }')
     for entry in "${a[@]}"; do
       IFS="=" read -r label mean_intensity <<< "$entry"
       label2mean_intensity+=([$label]=${mean_intensity})
     done
   else
     if [[ ! -f "${vol_stats_file}" ]]; then
-      Warning "Volumetric processing requested but did not find subcortical volume file: skipping"; return
+      SHOW_WARNING "Subject ${BIDS_ID}: '${feat}' not available. Skipping..."; return
     fi
   fi
 
@@ -226,18 +210,19 @@ do_subcortex_mapping (){
   printf -v csv_line "%s," "${result[@]}"
   printf "%s\n" "${csv_line%?}" >> "${output_file}" # Remove last comma and add newline
 
-  [[ -f "$output_file" ]] && n_steps=$((n_steps + 1))
+  [[ -f "$output_file" ]] && SHOW_NOTE "Subject ${BIDS_ID}: '${feat}' successfully mapped to subcortex.";
 }
 
 do_hippocampus_mapping() {
   local feat=$1
   local resolution=$2
 
-  Info "Map '${feat}' to hippocampus [resolution=${resolution}]"
+  SHOW_INFO "Map '${feat}' to hippocampus [resolution=${resolution}]"
 
   # Input & output locations
-  input_dir="${DIR_MAPS}"
-  output_dir=${SUBJECT_OUT}/${FOLDER_MAPS}/${FOLDER_HIPP}
+  surf_dir=${SUBJECT_HIPPUNFOLD_DIR}/surf
+  input_dir=${SUBJECT_MICAPIPE_DIR}/maps
+  output_dir=${SUBJECT_OUTPUT_DIR}/${FOLDER_MAPS}/${FOLDER_HIPP}
 
   # Mappings from features names to the way they appear in the input, intermediate and output filenames
   declare -A map_input=([thickness]=thickness [flair]='map-flair' [adc]='model-DTI_map-ADC' [fa]='model-DTI_map-FA' [qt1]='map-T1map')
@@ -254,10 +239,10 @@ do_hippocampus_mapping() {
   do
 
     # Set paths
-    surf_file="${SUBJECT_HIPP}/surf/${BIDS_ID}_hemi-${h}_space-T1w_den-${resolution}_label-hipp_midthickness.surf.gii"
+    surf_file="${surf_dir}/${BIDS_ID}_hemi-${h}_space-T1w_den-${resolution}_label-hipp_midthickness.surf.gii"
     input_file="${input_dir}/${BIDS_ID}_space-nativepro_${input_feat}.nii.gz" # Not used for thickness
     if [[ "$feat" == "thickness" ]]; then
-      inter_file="${SUBJECT_HIPP}/surf/${BIDS_ID}_hemi-${h}_space-T1w_den-${resolution}_label-hipp_thickness.shape.gii"
+      inter_file="${surf_dir}/${BIDS_ID}_hemi-${h}_space-T1w_den-${resolution}_label-hipp_thickness.shape.gii"
     else
       inter_file="${tmp_dir}/${BIDS_ID}_hemi-${h}_space-T1w_desc-${inter_feat}_den-${resolution}_feature-hipp_midthickness.func.gii"
     fi
@@ -266,20 +251,20 @@ do_hippocampus_mapping() {
     # Check if file exists
     [[ "$feat" != "thickness" ]] && check_file=${input_file} || check_file=${inter_file}
     if [[ ! -f "${check_file}" ]]; then
-      Warning "Subject ${BIDS_ID}: '${feat}' not available. Skipping..."
+      SHOW_WARNING "Subject ${BIDS_ID}: '${feat}' not available. Skipping..."
       break
     fi
 
     # Perform mapping
     if [[ "$feat" != "thickness" ]]; then
-      Do_cmd wb_command -volume-to-surface-mapping "${input_file}" "${surf_file}" "${inter_file}" -trilinear
+      DO_CMD wb_command -volume-to-surface-mapping "${input_file}" "${surf_file}" "${inter_file}" -trilinear
     fi
-    Do_cmd wb_command -metric-smoothing "${surf_file}" "${inter_file}" "${fwhm}" "${output_file}"
+    DO_CMD wb_command -metric-smoothing "${surf_file}" "${inter_file}" "${fwhm}" "${output_file}"
 
-    [[ -f "${output_file}" ]] && n=$((n + 1)) && n_steps=$((n_steps + 1))
+    [[ -f "${output_file}" ]] && n=$((n + 1))
   done
 
-  [[ $n -eq 2 ]] && Note "Subject ${BIDS_ID}: '${feat}' [resolution=${resolution}] successfully mapped to hippocampus.";
+  [[ $n -eq 2 ]] && SHOW_NOTE "Subject ${BIDS_ID}: '${feat}' [resolution=${resolution}] successfully mapped to hippocampus.";
 }
 
 
@@ -313,5 +298,5 @@ done
 
 #------------------------------------------------------------------------------#
 # Wrap up
-Title "${structure^} feature mapping ended in \033[38;5;220m $(bc <<< "scale=2; $SECONDS/60") minutes \033[38;5;141m.
-\tCheck logs      : $([ -n "$logfile" ] && echo "${logfile}" || echo "${DIR_LOGS}")"
+SHOW_TITLE "${structure^} feature mapping ended in \033[38;5;220m $(bc <<< "scale=2; $SECONDS/60") minutes \033[38;5;141m.
+\tCheck logs      : $([ -n "$logfile" ] && echo "${logfile}")"
--- functions/utilities.sh
+++ functions/utilities.sh
@@ -1,274 +1,7 @@
 #!/bin/bash
 
-#source ../config.cfg
 
-
-bids_variables() {
-  # This functions assigns variable names acording to:
-  #     Subject ID =                        $1
-  #     Raw data directory =                $2
-  #     Hippunfold directory derivatives =  $3
-  #     Micapipe derivatives directory =    $4
-  #     Z-brains derivatives directory =    $5
-  #     Session ID (optional) =             $6
-  id=$1 # id in the for HC001
-  raw_dir=$2
-  hipp_dir=$3
-  micapipe_dir=$4
-  out_dir=$5
-  session=$6
-  umask 002
-
-  # Define utilities/functions directory
-  export SCRIPT_DIR=${ZBRAINS}/functions
-
-  export SUBJECT=sub-${id}
-
-  # Handle Single Session
-  if [ "$session" == "SINGLE" ]; then
-      export SUBJECT_RAW=${raw_dir}/${SUBJECT}           # subject's raw data directory
-      export SUBJECT_HIPP=${hipp_dir}/hippundold/${SUBJECT}         # subject's hippunfold directory
-      export SUBJECT_MICAPIPE=${micapipe_dir}/${SUBJECT} # subject's micapipe directory
-      export SUBJECT_OUT=${out_dir}/${SUBJECT}           # subject's output directory
-      session_suffix=""
-  else
-    predix_session="ses-${session/ses-/}";
-    export SUBJECT_RAW=${raw_dir}/${SUBJECT}/${predix_session}           # subject's raw data directory
-    export SUBJECT_HIPP=${hipp_dir}/hippunfold/${SUBJECT}/${predix_session}         # subject's hippunfold directory
-    export SUBJECT_MICAPIPE=${micapipe_dir}/${SUBJECT}/${predix_session} # subject's micapipe directory
-    export SUBJECT_OUT=${out_dir}/${SUBJECT}/${predix_session}           # subject's output directory
-    session_suffix="_${predix_session}"
-  fi
-
-  export BIDS_ID="${SUBJECT}${session_suffix}"
-
-  # Structural directories derivatives/
-  export PROC_STRUCT=$SUBJECT_MICAPIPE/anat  # Structural processing directory
-  export DIR_VOLUM=$SUBJECT_MICAPIPE/parc    # Cortical segmentations
-  export DIR_CONTE69=$SUBJECT_MICAPIPE/surf  # Conte69 surfaces
-  export DIR_MAPS=$SUBJECT_MICAPIPE/maps
-
-  export DIR_WARP=$SUBJECT_MICAPIPE/xfm     # Transformation matrices
-  export DIR_LOGS=$SUBJECT_OUT/logs         # directory where log files will be written
-  export DIR_QC=$SUBJECT_MICAPIPE/QC        # directory with micapipe qc files
-
-  # post structural Files (the resolution might vary depending on the dataset)
-  RES=$(mrinfo "${PROC_STRUCT}"/"${BIDS_ID}"_space-nativepro_T1w.nii.gz -spacing | awk '{printf "%.1f\n", $2}')
-  export RES
-  export T1NATIVEPRO=${PROC_STRUCT}/${BIDS_ID}_space-nativepro_T1w.nii.gz
-  export T1NATIVEPRO_BRAIN=${PROC_STRUCT}/${BIDS_ID}_space-nativepro_T1w_brain.nii.gz
-  export T1NATIVEPRO_MASK=${PROC_STRUCT}/${BIDS_ID}_space-nativepro_T1w_brain_mask.nii.gz
-  export T1FAST_SEG=${DIR_VOLUM}/${BIDS_ID}_space-nativepro_T1w_atlas-subcortical.nii.gz
-
-  # raw files
-  RAW_FLAIR=$(ls "$SUBJECT_RAW"/anat/*FLAIR*.nii* 2>/dev/null)
-  export RAW_FLAIR
-}
-
-set_surface_directory() {
-  local recon=${1}
-  DIR_SURF=$(dirname "${out_dir}")/${recon}    # surf
-  export DIR_SURF
-  export DIR_SUBJSURF=${DIR_SURF}/${BIDS_ID}  # Subject surface dir
-  export T1SURF=${DIR_SUBJSURF}/mri/orig.mgz
-
-  # Native midsurface in gifti format
-  export LH_MIDSURF="${DIR_CONTE69}/${BIDS_ID}_hemi-L_surf-fsnative_label-midthickness.surf.gii"
-  export RH_MIDSURF="${DIR_CONTE69}/${BIDS_ID}_hemi-R_surf-fsnative_label-midthickness.surf.gii"
-}
-
-bids_print.variables-ctx() {
-  # This functions prints BIDS variables names and files if found
-  Info "Cortical processing variables:"
-  Note "T1 nativepro         :" "$(find "$T1NATIVEPRO" 2>/dev/null)"
-  Note "T1 resolution        :" "$RES"
-  Note "fs-LR-32k-lh surface :" "$(find "${DIR_CONTE69}/${BIDS_ID}_hemi-L_space-nativepro_surf-fsLR-32k_label-white.surf.gii" 2>/dev/null)"
-  Note "fs-LR-32k-rh surface :" "$(find "${DIR_CONTE69}/${BIDS_ID}_hemi-R_space-nativepro_surf-fsLR-32k_label-white.surf.gii" 2>/dev/null)"
-
-}
-
-bids_print() {
-  # This functions prints BIDS variables names and files if found
-
-  struct=$1
-
-  case $struct in
-    "cortex")
-      Info "Cortical processing variables:"
-      Note "T1 nativepro         :" "$(find "$T1NATIVEPRO" 2>/dev/null)"
-      Note "T1 resolution        :" "$RES"
-      Note "fs-LR-32k-lh surface :" "$(find "${DIR_CONTE69}/${BIDS_ID}_hemi-L_space-nativepro_surf-fsLR-32k_label-white.surf.gii" 2>/dev/null)"
-      Note "fs-LR-32k-rh surface :" "$(find "${DIR_CONTE69}/${BIDS_ID}_hemi-R_space-nativepro_surf-fsLR-32k_label-white.surf.gii" 2>/dev/null)"
-      ;;
-    "subcortex")
-      Info "Subcortical processing variables:"
-      Note "T1 nativepro    :" "$(find "$T1NATIVEPRO" 2>/dev/null)"
-      Note "T1 sctx seg     :" "$(find "$T1FAST_SEG" 2>/dev/null)"
-      Note "Sctx volume     :" "$(find "$DIR_SUBJSURF/stats/aseg.stats" 2>/dev/null)"
-      Note "T1 resolution   :" "$RES"
-      ;;
-    "hippocampus")
-      Info "Hippocampal processing variables:"
-      Note "T1 nativepro    :" "$(find "$T1NATIVEPRO" 2>/dev/null)"
-      Note "T1 resolution   :" "$RES"
-      ;;
-    *)  # Default case if none of the above patterns match
-      echo "Unknown structure in bids_print."
-      ;;
-  esac
-}
-
-bids_print.variables-hipp() {
-  # This functions prints BIDS variables names and files if found
-  Info "Hippocampal processing variables:"
-  Note "T1 nativepro    :" "$(find "$T1NATIVEPRO" 2>/dev/null)"
-  Note "T1 resolution   :" "$RES"
-}
-
-bids_variables_unset() {
-  # This function unsets all environment variables defined by bids_variables
-  unset SCRIPT_DIR
-  unset SUBJECT
-  unset SUBJECT_RAW
-  unset SUBJECT_HIPP
-  unset SUBJECT_MICAPIPE
-  unset SUBJECT_OUT
-  unset PROC_STRUCT
-  unset DIR_VOLUM
-  unset DIR_CONTE69
-  unset DIR_MAPS
-  unset DIR_WARP
-  unset DIR_LOGS
-  unset DIR_QC
-  unset RES
-  unset T1NATIVEPRO
-  unset T1NATIVEPRO_BRAIN
-  unset T1NATIVEPRO_MASK
-  unset T1FAST_SEG
-  unset RAW_FLAIR
-  unset DIR_SURF
-  unset DIR_SUBJSURF
-  unset T1SURF
-  unset LH_MIDSURF
-  unset RH_MIDSURF
-}
-
-function zbrains_json() {
-  # Name is the name of the raw-BIDS directory
-  if [ -f "${BIDS}/dataset_description.json" ]; then
-    Name=$(grep Name "${BIDS}"/dataset_description.json | awk -F '"' '{print $4}')
-    BIDSVersion=$(grep BIDSVersion "${BIDS}"/dataset_description.json | awk -F '"' '{print $4}')
-  else
-    Name="BIDS dataset_description NOT found"
-    BIDSVersion="BIDS dataset_description NOT found"
-  fi
-
-  echo -e "{
-    \"Name\": \"${Name}\",
-    \"BIDSVersion\": \"${BIDSVersion}\",
-    \"DatasetType\": \"derivative\",
-    \"GeneratedBy\": [{
-        \"Name\": \"z-brains\",
-        \"Version\": \"${VERSION}\",
-        \"Reference\": \"tbd\",
-        \"DOI\": \"tbd\",
-        \"URL\": \"https://z-brains.readthedocs.io\",
-        \"GitHub\": \"https://github.com/MICA-MNI/z-brains\",
-        \"Container\": {
-          \"Type\": \"tbd\",
-          \"Tag\": \"ello:zbrains:$(echo "${VERSION}" | awk '{print $1}')\"
-        },
-        \"RunBy\": \"$(whoami)\",
-        \"Workstation\": \"$(uname -n)\",
-        \"LastRun\": \"$(date)\",
-        \"Processing\": \"${PROC}\"
-      }]
-  }" > "${out_dir}/dataset_description.json"
-}
-
-zbrains_software() {
-  Info "Z-BRAINS - Software versions"
-  Note "ANTS........" "$(antsRegistration --version | awk -F ':' 'NR==1{print $2}')"
-  Note "            " "$ANTSPATH"
-  Note "WorkBench..." "$(wb_command -version | awk 'NR==3{print $2}')"
-  Note "            " "$(which wb_command)"
-  Note "python......" "$(python --version | awk 'NR==1{print $2}')"
-  Note "            " "$(which python)"
-  Note "conda......." "$(conda --version)"
-  Note "            " "$(which conda)"
-}
-
-function cleanup() {
-  # This script will clean the temporary directory
-  # and reset the old user path upon interrupt and termination.
-  tmp_dir=$1
-#  nocleanup=$2
-
-  rm -Rf "${tmp_dir:?}" 2>/dev/null
-  bids_variables_unset
-
-#  # Clean temporary directory and temporary fsaverage5
-#  if [[ $nocleanup == "FALSE" ]]; then
-#      rm -Rf "${tmp_dir:?}" 2>/dev/null
-#  else
-#      echo -e "z-brains tmp directory was not erased: \n\t\t${tmp_dir}";
-#  fi
-##  cd "$here" || exit
-#  bids_variables_unset
-  if [[ -n "$OLD_PATH" ]]; then export PATH=$OLD_PATH; unset OLD_PATH; fi
-}
-
-
-# ----------------------------------------------------------------------------------------------- #
-# ------------------------------- Printing and display functions -------------------------------- #
-# ----------------------------------------------------------------------------------------------- #
-# The following functions are only to print on the terminal colorful messages:
-#     Error messages
-#     Warning messages
-#     Note messages
-#     Warn messages
-#     Title messages
-
-COLOR_ERROR="\033[38;5;9m"
-COLOR_NOTE="\033[38;5;122m"
-COLOR_INFO="\033[38;5;75m"
-COLOR_WARNING="\033[38;5;184m"
-COLOR_TITLE="\033[38;5;141m"
-COLOR_NOTE="\033[0;36;10m"
-NC="\033[0m" # No color
-
-Error() {
-echo -e "${COLOR_ERROR}\n-------------------------------------------------------------\n\n[ ERROR ]..... $1\n
--------------------------------------------------------------${NC}\n"
-}
-Note() {
-if [[ ${VERBOSE} -gt 1 || ${VERBOSE} -lt 0 ]]; then echo -e "\t\t$1\t${COLOR_NOTE}$2 ${NC}"; fi
-}
-Info() {
-if [[ ${VERBOSE} -gt 1 || ${VERBOSE} -lt 0 ]]; then echo  -e "${COLOR_INFO}\n[ INFO ]..... $1 ${NC}"; fi
-}
-Warning() {
-if [[ ${VERBOSE} -gt 0 || ${VERBOSE} -lt 0 ]]; then echo  -e "${COLOR_WARNING}\n[ WARNING ]..... $1 ${NC}"; fi
-}
-Warn() {
-if [[ ${VERBOSE} -gt 0 || ${VERBOSE} -lt 0 ]]; then echo  -e "${COLOR_WARNING}
--------------------------------------------------------------\n
-[ WARNING ]..... $1
-\n-------------------------------------------------------------${NC}"; fi
-}
-Title() {
-if [[ ${VERBOSE} -gt 1 || ${VERBOSE} -lt 0 ]]; then echo -e "\n${COLOR_TITLE}
--------------------------------------------------------------
-\t$1
--------------------------------------------------------------${NC}"; fi
-}
-
-
-# Export
-export -f Error Note Info Warning Warn Title
-
-
-function Do_cmd() {
+function DO_CMD() {
 # do_cmd sends command to stdout before executing it.
 str="$(whoami) @ $(uname -n) $(date)"
 local l_command=""
@@ -294,10 +27,92 @@ while [ ${l_index} -le $# ]; do
     l_index=$((l_index+1))
    done
 
-#[[ ${QUIET} != true ]] && echo -e "\033[38;5;118m\n${str}:\nCOMMAND -->  \033[38;5;122m${l_command}  \033[0m";
 if [[ ${VERBOSE} -gt 2 || ${VERBOSE} -lt 0 ]]; then
   echo -e "\033[38;5;118m\n${str}:\nCOMMAND -->  \033[38;5;122m${l_command}  \033[0m";
 fi
-if [ -z "$TEST" ]; then $l_command; fi
+
+$l_command
 }
+
+
+#function zbrains_json() {
+#  # Name is the name of the raw-BIDS directory
+#  if [ -f "${BIDS}/dataset_description.json" ]; then
+#    Name=$(grep Name "${BIDS}"/dataset_description.json | awk -F '"' '{print $4}')
+#    BIDSVersion=$(grep BIDSVersion "${BIDS}"/dataset_description.json | awk -F '"' '{print $4}')
+#  else
+#    Name="BIDS dataset_description NOT found"
+#    BIDSVersion="BIDS dataset_description NOT found"
+#  fi
+#
+#  echo -e "{
+#    \"Name\": \"${Name}\",
+#    \"BIDSVersion\": \"${BIDSVersion}\",
+#    \"DatasetType\": \"derivative\",
+#    \"GeneratedBy\": [{
+#        \"Name\": \"z-brains\",
+#        \"Version\": \"${VERSION}\",
+#        \"Reference\": \"tbd\",
+#        \"DOI\": \"tbd\",
+#        \"URL\": \"https://z-brains.readthedocs.io\",
+#        \"GitHub\": \"https://github.com/MICA-MNI/z-brains\",
+#        \"Container\": {
+#          \"Type\": \"tbd\",
+#          \"Tag\": \"ello:zbrains:$(echo "${VERSION}" | awk '{print $1}')\"
+#        },
+#        \"RunBy\": \"$(whoami)\",
+#        \"Workstation\": \"$(uname -n)\",
+#        \"LastRun\": \"$(date)\",
+#        \"Processing\": \"${PROC}\"
+#      }]
+#  }" > "${out_dir}/dataset_description.json"
+#}
+
+
+# ----------------------------------------------------------------------------------------------- #
+# ------------------------------- Printing and display functions -------------------------------- #
+# ----------------------------------------------------------------------------------------------- #
+# The following functions are only to print on the terminal colorful messages:
+#     Error messages
+#     Warning messages
+#     Note messages
+#     Info messages
+#     Title messages
+# VERBOSE is defined by the z-brains main script
+
+color_error="\033[38;5;9m"
+color_info="\033[38;5;75m"
+color_warning="\033[38;5;184m"
+color_title="\033[38;5;141m"
+color_note="\033[0;36;10m"
+no_color="\033[0m" # No color
+
+SHOW_ERROR() {
+echo -e "${color_error}\n-------------------------------------------------------------\n\n[ ERROR ]..... $1\n
+-------------------------------------------------------------${no_color}\n"
+}
+
+SHOW_WARNING() {
+  if [[ ${VERBOSE} -gt 0 || ${VERBOSE} -lt 0 ]]; then echo  -e "${color_warning}\n[ WARNING ]..... $1 ${no_color}"; fi
+}
+
+SHOW_NOTE() {
+  if [[ ${VERBOSE} -gt 1 || ${VERBOSE} -lt 0 ]]; then echo -e "\t\t$1\t${color_note}$2 ${no_color}"; fi
+}
+
+SHOW_INFO() {
+  if [[ ${VERBOSE} -gt 1 || ${VERBOSE} -lt 0 ]]; then echo  -e "${color_info}\n[ INFO ]..... $1 ${no_color}"; fi
+}
+
+SHOW_TITLE() {
+if [[ ${VERBOSE} -gt 1 || ${VERBOSE} -lt 0 ]]; then echo -e "\n${color_title}
+-------------------------------------------------------------
+\t$1
+-------------------------------------------------------------${no_color}"
+fi
+}
+
+
+# Export
+export -f DO_CMD SHOW_ERROR SHOW_WARNING SHOW_NOTE SHOW_INFO SHOW_TITLE
 
