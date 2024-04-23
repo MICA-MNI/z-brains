00cd38662723d8ff133604bfcc4007eb4b99d992
Ian-Goodall-Halliwell <90027009+Ian-Goodall-Halliwell@users.noreply.github.com>
2024-04-22 20:11:39 -0400

    Update run_analysis.py

--- functions/run_analysis.py
+++ functions/run_analysis.py
@@ -313,7 +313,8 @@ def main(zbrains_ref,demo_ref,column_map,subject_id,session,demo,zbrains,struct,
         feat_report.append(multi)
 
     print('feat_report={}'.format(feat_report))
-
+    print(feat_report)
+    exit()
     tmp = '/tmp' if tmp is None else tmp
     generate_clinical_report(
         zbrains_path=zbrains, sid=px_id, ses=px_ses, age=age, sex=sex,
